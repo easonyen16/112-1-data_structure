c61c7dbd26ca72de0cdc5a4b71463cc1aace8c59
easonyen16 <eason990024@gmail.com>
2023-12-19 16:19:06 +0800

    Update heap-1.c

--- final-exam/heap-1.c
+++ final-exam/heap-1.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAXNODES 6
-int hasHeap() {
+int hasHeap(){
     int n;
     scanf("%d", &n);
     return n;
 }
-
-int* loadHeap(int n) {
+int *loadHeap(int n){
     int size = sizeof(int) * (n+1);
     int* a = (int*) malloc(size);
     int i;
     for (i=1; i<=n; ++i) {
         scanf("%d", &a[i]);
-        printf("allocatc %d bytes of an integer array for a heap from a[1] to a[%d].\n", size, n);
     }
+    printf("allocate %d bytes of an integer array for a heap from a[1] to a[%d].\n", size, n);
     return a;
+}
+void freeHeap(int arr[]){
+    free(arr);
 }
 void dumpHeap(char msg[], int a[] , int n, int width){
     int i;
     printf("%s\n", msg);
     for (i=1; i<=n; ++i) {
         if(i>1 && i%width==1){
-            printf("*****\n");
+            printf("\n");
         }
         if(i%width==1){
             printf("a[%02d]~a[%02d]:", i, (i+width-1>n?n:i+width-1));
@@ -31,15 +33,16 @@ void dumpHeap(char msg[], int a[] , int n, int width){
     }
     printf("\n");
 }
-int main() {
+int main(){
     int nodes, caseno=0;
     int* arr;
-    while ((nodes=hasHeap())) {
+    while((nodes=hasHeap())){
         if(caseno>0){
             printf("\n");
         }
         printf("Case #%d: there are %d unsorted integers.\n", ++caseno, nodes);
         arr = loadHeap(nodes);
         dumpHeap("input array:", arr, nodes, MAXNODES);
+        freeHeap(arr);
     }
 }
